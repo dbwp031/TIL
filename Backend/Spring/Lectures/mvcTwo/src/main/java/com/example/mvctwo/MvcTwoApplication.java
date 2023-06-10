989d3b22dff1e7f4b43c258d9e09d0fa75892a2e
Yuje Lee <dbwp031@khu.ac.kr>
2023-06-10 19:38:30 +0900

    mvcTwo first commit

---
+++ Backend/Spring/Lectures/mvcTwo/src/main/java/com/example/mvctwo/MvcTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTwoApplication.class, args);
+    }
+
+}
