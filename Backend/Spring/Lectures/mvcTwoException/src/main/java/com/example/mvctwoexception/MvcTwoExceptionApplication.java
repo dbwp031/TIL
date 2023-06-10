1348e8beb67964d65680e5dba6e6739031db49c0
Yuje Lee <dbwp031@khu.ac.kr>
2023-06-10 22:19:03 +0900

    update mvcTwo

---
+++ Backend/Spring/Lectures/mvcTwoException/src/main/java/com/example/mvctwoexception/MvcTwoExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctwoexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTwoExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTwoExceptionApplication.class, args);
+    }
+
+}
